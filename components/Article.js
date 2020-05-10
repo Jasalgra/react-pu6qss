ffaebb2a74f8274dcdd825315a2a6e85b5405acc
Jasalgra <jasalgra@gmail.com>
2020-05-10 23:52:37 +0300

    create components

--- components/Article.js
+++ components/Article.js
@@ -1,24 +1,47 @@
-import React from 'react'
+import React, {Component} from 'react'
+
+class Article extends Component {
+  constructor(props) {
+    super(props) 
+
+    this.state = {
+      isOpen: false
+    }
+  }
+
+  render() {
+    const {article} = this.props
+    console.log('---', this.props)
+    let bodylet = this.state.isOpen && <section>{article.text}</section>
+    return (
+      <div className='hello' style={{color: 'red'}}>
+        <h2>
+          {article.title}
+          <button onClick={this.handleClick}>
+            {this.state.isOpen ? "close" : "open"}
+          </button>
+        </h2>
+        {bodylet}
+        <h3>creation data: {(new Date(article.date)).toDateString()}</h3>
+      </div>
+    ) 
+  }
+
+  handleClick = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    })
+  }
+}
 
 function Article(props) {
-  const {article} = props
-  console.log('---', props)
-  let bodylet = <section>{article.text}</section>
-  return (
-    <div className='hello' style={{color: 'red'}}>
-      <h2>
-        {article.title}
-        <button onClick={handleClick}>close</button>
-      </h2>
-      {bodylet}
-      <h3>creation data: {(new Date(article.date)).toDateString()}</h3>
-    </div>
-  )
+  
 }
 
 function handleClick() {
   console.log('---', "clicked")
-  
+
+
 }
 
 export default  Article
